83e5a1803e242a3008123e87c38d1000e3133c7d
willofD13 <leonidastsiakas13@gmail.com>
2023-11-13 17:29:02 +0200

    Set up bi-directional associations between models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, length: {max: 15}
   validates :body, presence: true, length: {max: 50}
