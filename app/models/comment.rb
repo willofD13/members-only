44dc6790d69be193e7f6613e402f2d4ceb5c7df7
willofD13 <leonidastsiakas13@gmail.com>
2023-11-13 20:26:23 +0200

    Fix typo

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :body, presence: true, length: {max: 50}
+  validates :body, presence: true, length: {maximum: 50}
 end
