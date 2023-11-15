1fc7e3eaf56f14cd78ccad2feb484ef1c3e02462
willofD13 <leonidastsiakas13@gmail.com>
2023-11-15 15:25:24 +0200

    Delete confirmable and lockable modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable
+         :recoverable, :rememberable, :validatable
+    
     has_many :posts
     has_many :comments
 
