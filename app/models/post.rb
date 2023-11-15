67de3aae4dc47cac4350c2844a050248e943477d
willofD13 <leonidastsiakas13@gmail.com>
2023-11-15 20:24:49 +0200

    Change post title maximum length

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :title, presence: true, length: {maximum: 15}
+  validates :title, presence: true, length: {maximum: 20}
   validates :body, presence: true, length: {maximum: 50}
 end
