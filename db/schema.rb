eb82b70daa6c9298c607a8596a0e15bb7f7eee4f
willofD13 <leonidastsiakas13@gmail.com>
2023-11-15 20:32:02 +0200

    Create a home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_14_145933) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_15_182740) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_14_145933) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
