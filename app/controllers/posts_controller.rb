0b0eeafe4795ab571a3443b570b4bb0459f25d11
willofD13 <leonidastsiakas13@gmail.com>
2023-11-15 21:33:28 +0200

    Correct find method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,11 +20,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find_by(:id)
+    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find_by(:id)
+    @post = Post.find(params[:id])
 
     if @post.update(post_params)
       redirect_to posts_path
