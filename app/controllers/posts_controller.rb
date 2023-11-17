0a662b2bedbff4ef3db18aaa372f9d5cafce86ac
willofD13 <leonidastsiakas13@gmail.com>
2023-11-17 19:34:19 +0200

    Add destroy post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,14 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to root_path
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:title,:body,:user_id)
