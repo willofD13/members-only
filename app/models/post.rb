b52727e5e9b2a7caa924adb6bd41806498f37c95
willofD13 <leonidastsiakas13@gmail.com>
2023-11-17 19:32:16 +0200

    Change title length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :title, presence: true, length: {maximum: 20}
+  validates :title, presence: true, length: {maximum: 30}
   validates :body, presence: true, length: {maximum: 50}
 end
