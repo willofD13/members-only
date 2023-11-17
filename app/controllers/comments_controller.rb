6153abbe113bb151e29df37ebcf31d6781da08a1
willofD13 <leonidastsiakas13@gmail.com>
2023-11-17 19:50:16 +0200

    Add create function fro comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,5 +7,17 @@ class CommentsController < ApplicationController
         @comment = Comment.new
     end
 
+    def create
+        @comment = Comment.new(comment_params)
 
+        if @comment.save
+            redirect_to posts_path, status: :see_other
+        else
+            redirect_to posts_path, status: :unprocessable_entity
+        end
+    end
+
+    def comment_params
+        params.require(:comment).permit(:body,:user_id,:post_id)
+    end
 end
