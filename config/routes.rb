c94d49320cf87f819c7c4964a739ebd9d98437d5
willofD13 <leonidastsiakas13@gmail.com>
2023-11-18 13:11:03 +0200

    Create nested resource for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   resources :users
-  resources :comments
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
