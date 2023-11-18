73a20cc2961855967ffa3ff188219ef622081e62
willofD13 <leonidastsiakas13@gmail.com>
2023-11-18 14:07:16 +0200

    Create a comment linked to a particular post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,16 +8,12 @@ class CommentsController < ApplicationController
     end
 
     def create
-        @comment = Comment.new(comment_params)
-
-        if @comment.save
-            redirect_to posts_path, status: :see_other
-        else
-            redirect_to posts_path, status: :unprocessable_entity
-        end
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        redirect_to post_path(@post)
     end
 
     def comment_params
-        params.require(:comment).permit(:body,:user_id,:post_id)
+        params.require(:comment).permit(:body,:user_id)
     end
 end
